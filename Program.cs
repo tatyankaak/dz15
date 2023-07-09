56d0707ae404fcea9440fd636ef19616f2234610
Татьяна <robsten9086@mail.ru>
2023-07-09 18:08:15 +0300

    задача 15

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите цифру, обозначающую день недели:");
+        int day = int.Parse(Console.ReadLine());
+
+        if (day == 6 || day == 7)
+        {
+            Console.WriteLine("День является выходным.");
+        }
+        else
+        {
+            Console.WriteLine("День не является выходным.");
+        }
+    }
+}
